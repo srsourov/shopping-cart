cf38de155493f02c1e31973b15be19dddd110557
SR Sourov <sourov.web@gmail.com>
2021-09-06 23:18:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product + "-number");
+    let productNumber = productInput.value;
+    if (isIncreasing == true){
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0){
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + "-total");
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculatTotal();
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + "-number");
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculatTotal(){
+
+    const phoneTotal = getInputValue("phone") * 1219;
+    const caseTotal = getInputValue("case") * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxTotal = subTotal / 10;
+    const totalPrice = subTotal + taxTotal;
+    // update on the html
+    document.getElementById("sub-total").innerText = subTotal;
+    document.getElementById("tax-amount").innerText = taxTotal;
+    document.getElementById("total-price").innerText = totalPrice;
+}
+
+// handle phone prices
+document.getElementById("phone-plus").addEventListener("click", function(){
+    updateProductNumber("phone",1219, true)
+})
+
+document.getElementById("phone-minus").addEventListener("click", function(){
+    updateProductNumber("phone",1219, false)
+})
+
+// handle case prices
+document.getElementById("case-plus").addEventListener("click", function(){
+    updateProductNumber("case",59, true)
+})
+
+document.getElementById("case-minus").addEventListener("click", function(){
+    updateProductNumber("case",59, false)
+})
